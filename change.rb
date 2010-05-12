c533c06abb3e9438d9c4ce3f3c9f9d057dc876fe
fuJiin <fu7iin@gmail.com>
2010-05-11 23:15:46 -0700

    Initial commit for MongoMapper

--- change.rb
+++
@@ -1,7 +0,0 @@
-require "yaml"
-
-@db = File.open("database.yml", "r")
-@data = YAML.load(@db)
-@data["users"]["elpizo"]["name"] = "keke"
-
-File.open("database.yml", "w") {|data| data << @data.to_yaml}
